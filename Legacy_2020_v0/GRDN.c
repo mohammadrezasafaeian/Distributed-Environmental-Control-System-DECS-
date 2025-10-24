2198426e0e5b19bf007e52b41bb0d32380bd309c
mohammadrezasafaeian <157393233+mohammadrezasafaeian@users.noreply.github.com>
2025-10-24 18:36:50 +0330

    Update GRDN.c

--- Legacy_2020_v0/GRDN.c
+++ Legacy_2020_v0/GRDN.c
@@ -2,12 +2,12 @@
 This program was created by the
 CodeWizardAVR V3.12 Advanced
 Automatic Program Generator
-© Copyright 1998-2014 Pavel Haiduc, HP InfoTech s.r.l.
+Â© Copyright 1998-2014 Pavel Haiduc, HP InfoTech s.r.l.
 http://www.hpinfotech.com
 
 Project : 
 Version : 
-Date    : 26/09/2016
+Date    : 26/09/2020
 Author  : 
 Company : 
 Comments: 
